3d08996730c6420d523344de46fd2454d49cb0ce
Krishnan S <62713092+skrishnan2001@users.noreply.github.com>
2020-12-20 19:19:37 +0530

    Changed class components to functional components

--- screens/InvoiceScreen.js
+++ screens/InvoiceScreen.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, Component } from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
-import FormInput from "../components/FormInput";
+import { db } from "../firebaseConfig";
 import { AuthContext } from "../navigation/AuthProvider";
 import FormButton from "../components/FormButton";
 import {
@@ -12,79 +12,89 @@ import {
 } from "react-native-table-component";
 import * as firebase from "firebase";
 
-export default class InvoiceScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ["", "Details"],
-      tableTitle: [
-        "Pickup Address",
-        "Delivery Address",
-        "Phone number",
-        "Category",
-        "Dimension",
-        "Weight",
-        "Type",
-        "Order Value",
-        "Insurance",
-      ],
-      tableData: [
-        ["49 street"],
-        ["67 Street"],
-        ["123456789"],
-        ["Bulk"],
-        ["25*35*68"],
-        ["45"],
-        ["Elect"],
-        ["100"],
-        ["Yes"],
-      ],
-    };
-  }
-  render() {
-    var ref = firebase.database().ref();
-    ref.on(
-      "value",
-      function (snapshot) {
-        console.log(snapshot.val());
-      },
-      function (error) {
-        console.log("Error: " + error.code);
-      }
-    );
-    const state = this.state;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.top}>Invoice</Text>
-        <Table borderStyle={{ borderWidth: 2 }}>
-          <Row
-            data={state.tableHead}
-            flexArr={[1, 4.1, 1, 1]}
-            style={styles.head}
+const InvoiceScreen = ({ navigation }) => {
+  const { user } = useContext(AuthContext);
+  const [curr, next] = useState({
+    tableHead: ["", "Details"],
+    tableTitle: [
+      "Pickup Address",
+      "Delivery Address",
+      "Phone number",
+      "Category",
+      "Dimension",
+      "Weight",
+      "Type",
+      "Order Value",
+      "Insurance",
+    ],
+    tableData: [
+      ["49 street"],
+      ["67 Street"],
+      ["123456789"],
+      ["Bulk"],
+      ["25*35*68"],
+      ["45"],
+      ["Elect"],
+      ["100"],
+      ["Yes"],
+    ],
+  });
+
+  var ref = firebase.database().ref();
+  ref.on(
+    "value",
+    function (snapshot) {
+      console.log(snapshot.val());
+    },
+    function (error) {
+      console.log("Error: " + error.code);
+    }
+  );
+
+  var bookingRef = firebase.database().ref(`/users/booking/${user.uid}`);
+
+  bookingRef.on("child_added", function (data, prevChildKey) {
+    var newBooking = data.val();
+    console.log("Pick-up: " + newBooking.residence_locality_pickup);
+    console.log("Drop: " + newBooking.residence_locality_delivery);
+    console.log("Phone number: " + newBooking.phone);
+    console.log("Previous Player: " + prevChildKey);
+  });
+
+  const state = curr;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.top}>Invoice</Text>
+      <Table borderStyle={{ borderWidth: 2 }}>
+        <Row
+          data={state.tableHead}
+          flexArr={[1, 4.1, 1, 1]}
+          style={styles.head}
+          textStyle={styles.text}
+        />
+        <TableWrapper style={styles.wrapper}>
+          <Col
+            data={state.tableTitle}
+            style={styles.title}
             textStyle={styles.text}
           />
-          <TableWrapper style={styles.wrapper}>
-            <Col
-              data={state.tableTitle}
-              style={styles.title}
-              textStyle={styles.text}
-            />
-            <Rows
-              data={state.tableData}
-              flexArr={[2, 1, 1]}
-              style={styles.row}
-              textStyle={styles.text}
-            />
-          </TableWrapper>
-        </Table>
-        <FormButton
-          buttonTitle="Redirect to Fresh-Booking"
-          onPress={() => this.props.navigation.navigate("Booking")}
-        />
-      </View>
-    );
-  }
+          <Rows
+            data={state.tableData}
+            flexArr={[2, 1, 1]}
+            style={styles.row}
+            textStyle={styles.text}
+          />
+        </TableWrapper>
+      </Table>
+      <FormButton
+        buttonTitle="Redirect to Fresh-Booking"
+        onPress={() => navigation.navigate("Booking")}
+      />
+    </View>
+  );
 }
+
+export default InvoiceScreen;
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, paddingTop: 50, backgroundColor: "#fff" },
