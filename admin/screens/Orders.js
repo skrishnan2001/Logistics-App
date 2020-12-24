de39fe2d404bb3e67b5a848a0e7cd3af0d22eb16
Krishnan S <62713092+skrishnan2001@users.noreply.github.com>
2020-12-24 22:23:13 +0530

    Search filter for different criteria completed

--- admin/screens/Orders.js
+++ admin/screens/Orders.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import * as firebase from "firebase";
 import Cards from "../components/Cards";
-import { View, FlatList, TextInput } from "react-native";
+import {
+  View,
+  FlatList,
+  Picker,
+  StyleSheet,
+  Text,
+} from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import FormInput from "../components/FormInput";
 
@@ -22,7 +28,11 @@ const Orders = ({ navigation }) => {
               userid: key,
               orderid: key_2,
               type: val_2["type"],
-              weight: val_2["weight"],
+              category: val_2["PickerSelectedVal"],
+              pc_del: val_2["pincode_delivery"],
+              pc_pick: val_2["pincode_pickup"],
+              prior_booking: val_2["Priority_Booking"],
+              insurance: val_2["insurance"],
             };
             i++;
             users.push(user);
@@ -30,12 +40,14 @@ const Orders = ({ navigation }) => {
         }
       }
     }
-    //console.log(users);
+    console.log(users);
   });
   const [user_id, setuserid] = useState();
   const [order_id, setorderid] = useState();
   const [arrHolder, setarrHolder] = useState(users);
-  const [data_history, setdata_history] = useState(users);
+  // const [data_history, setdata_history] = useState(users);
+  var data_history = users;
+  const [PickerSelectedVal, setPickerSelectedVal] = useState("orderid");
   const Check = (user_id, order_id, item) => {
     setuserid(user_id);
     setorderid(order_id);
@@ -44,22 +56,46 @@ const Orders = ({ navigation }) => {
 
   const filter_func = (text) => {
     const newData = data_history.filter((items) => {
-      var order = items.orderid;
-      //console.log(order.includes(text));
+      var order = items[`${PickerSelectedVal}`];
+      if (
+        PickerSelectedVal == "prior_booking" ||
+        PickerSelectedVal == "insurance"
+      ) {
+        if (items[`${PickerSelectedVal}`]) order = "Yes";
+        else order = "No";
+      }
+      console.log(items[`${PickerSelectedVal}`]);
+      console.log(order.includes(text));
       return order.includes(text);
     });
     setarrHolder(newData);
   };
-  // console.log(data_history);
+  console.log(data_history);
   return (
     <View
       style={{
         backgroundColor: "#f8f4f4",
         padding: 20,
-        paddingTop: 50,
-        flex: 1
+        flex: 1,
       }}
     >
+      <Text style={[styles.text, { marginTop: 20 }]}>Search Filter</Text>
+      <Picker
+        selectedValue={PickerSelectedVal}
+        style={[styles.inputsingle, { height: 50, width: 200 }]}
+        onValueChange={(itemValue, itemIndex) =>
+          setPickerSelectedVal(itemValue)
+        }
+      >
+        <Picker.Item label="Order ID" value="orderid" style={styles.labelPicker} />
+        <Picker.Item label="User ID" value="userid" style={styles.labelPicker} />
+        <Picker.Item label="Category" value="category" style={styles.labelPicker} />
+        <Picker.Item label="Type" value="type" style={styles.labelPicker} />
+        <Picker.Item label="Insurance" value="insurance" style={styles.labelPicker} />
+        <Picker.Item label="Priority Booking" value="prior_booking" style={styles.labelPicker} />
+        <Picker.Item label="Pincode(Delivery)" value="pc_del" style={styles.labelPicker} />
+        <Picker.Item label="Pincode(Pickup)" value="pc_pick" style={styles.labelPicker} />
+      </Picker>
 
       <FormInput
         onChangeText={(text) => filter_func(text)}
@@ -70,7 +106,6 @@ const Orders = ({ navigation }) => {
       />
 
       <FlatList
-        style={{ marginTop: 10 }}
         data={arrHolder}
         keyExtractor={(user) => user.id}
         renderItem={({ item }) => (
@@ -80,9 +115,33 @@ const Orders = ({ navigation }) => {
             <Cards userId={item.userid} orderId={item.orderid} />
           </TouchableWithoutFeedback>
         )}
+        style={{ marginTop: 10 }}
       />
     </View>
   );
 };
 
 export default Orders;
+
+const styles = StyleSheet.create({
+  inputsingle: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 5,
+    marginTop: 10,
+    borderRadius: 125,
+    marginBottom: 5,
+    width: 250,
+    height: 30,
+    color: "#2e64e5",
+    backgroundColor: "#465881",
+  },
+  labelPicker: {
+    fontSize: 18,
+  },
+  text: {
+    color: "#051d5f",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
