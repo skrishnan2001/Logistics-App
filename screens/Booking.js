eb0731817fd706a3b1ed671719997b093889ae99
Krishnan S <62713092+skrishnan2001@users.noreply.github.com>
2020-12-30 18:49:10 +0530

    Update Booking.js

--- screens/Booking.js
+++ screens/Booking.js
@@ -108,39 +108,39 @@ const BookingScreen = ({ navigation }) => {
       isScheduled: false,
     });
 
-    var bookingRef = firebase.database().ref(`/users/booking/${user.uid}`);
-    var OrderID;
-    bookingRef.limitToLast(1).on("child_added", function (snapshot) {
-      OrderID = snapshot.key;
-    });
+  //   var bookingRef = firebase.database().ref(`/users/booking/${user.uid}`);
+  //   var OrderID;
+  //   bookingRef.limitToLast(1).on("child_added", function (snapshot) {
+  //     OrderID = snapshot.key;
+  //   });
 
-    var hashedID = user.uid;
-    db.ref(`/admin/booking/${zone}/`).push({
-      CustomerID: hashedID,
-      Order_ID: OrderID,
-      residence_locality_pickup: pickup,
-      city_pickup: str[0],
-      state_pickup: str[1],
-      pincode_pickup: pickup3,
-      residence_locality_delivery: delivery,
-      city_delivery: str1[0],
-      state_delivery: str1[1],
-      pincode_delivery: delivery3,
-      phone: phone,
-      PickerSelectedVal: PickerSelectedVal,
-      length: dimension,
-      breadth: dimension2,
-      height: dimension3,
-      weight: weight,
-      vehicle: vehicle_type,
-      type: type,
-      order: order,
-      insurance: check,
-      Priority_Booking: Priority,
-      Time: d,
-      zone: zone,
-    });
-  };
+  //   var hashedID = user.uid;
+  //   db.ref(`/admin/booking/${zone}/`).push({
+  //     CustomerID: hashedID,
+  //     Order_ID: OrderID,
+  //     residence_locality_pickup: pickup,
+  //     city_pickup: str[0],
+  //     state_pickup: str[1],
+  //     pincode_pickup: pickup3,
+  //     residence_locality_delivery: delivery,
+  //     city_delivery: str1[0],
+  //     state_delivery: str1[1],
+  //     pincode_delivery: delivery3,
+  //     phone: phone,
+  //     PickerSelectedVal: PickerSelectedVal,
+  //     length: dimension,
+  //     breadth: dimension2,
+  //     height: dimension3,
+  //     weight: weight,
+  //     vehicle: vehicle_type,
+  //     type: type,
+  //     order: order,
+  //     insurance: check,
+  //     Priority_Booking: Priority,
+  //     Time: d,
+  //     zone: zone,
+  //   });
+   };
 
   const VehiclePicker = () => {
     var volume = dimension * dimension2 * dimension3;
@@ -154,18 +154,18 @@ const BookingScreen = ({ navigation }) => {
       vehicle_type = "truck";
     }
   };
-  const zone_Allotment = () => {
-    var north = ["600081", "600011", "600060", "601204", "600019"];
-    var south = ["600020", "600016", "600004", "600042", "600087"];
-    var central = ["600017", "600018", "600005", "600018", "600040"];
-    if (north.includes(pickup3)) zone = "North";
-    else if (south.includes(pickup3)) zone = "South";
-    else if (central.includes(pickup3)) zone = "Central";
-    else zone = "Others";
-  };
+  // const zone_Allotment = () => {
+  //   var north = ["600081", "600011", "600060", "601204", "600019"];
+  //   var south = ["600020", "600016", "600004", "600042", "600087"];
+  //   var central = ["600017", "600018", "600005", "600018", "600040"];
+  //   if (north.includes(pickup3)) zone = "North";
+  //   else if (south.includes(pickup3)) zone = "South";
+  //   else if (central.includes(pickup3)) zone = "Central";
+  //   else zone = "Others";
+  // };
   const handleSubmit = () => {
     VehiclePicker();
-    zone_Allotment();
+    //zone_Allotment();
     var d = new Date();
     addItems(d.toString());
     alert("Order Placed Successfully");
