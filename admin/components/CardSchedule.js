da56ccab8e04ecd5a427e21ac32079d4c308fb65
vishnu-vasan <58689453+vishnu-vasan@users.noreply.github.com>
2021-01-02 13:55:44 +0530

    Add files via upload

--- admin/components/CardSchedule.js
+++ admin/components/CardSchedule.js
@@ -1,39 +1,8 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text, Image, Picker } from "react-native";
-import FormButton from "../components/FormButton";
-import { db } from "../firebaseConfig";
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
 
-function CardSchedule({ userId, orderId, genInvoice }) {
-  const [staff_picker, setstaff_picker] = useState("Staff 1");
-  var staff = {};
-  var dbRef = db.ref("/staff/ProfileDetails/");
-  dbRef.on("value", function (snapshot) {
-    const data = snapshot.val();
-    for (var key in data) {
-      if (data.hasOwnProperty(key)) {
-        var val = data[key];
-        for (var key_2 in val) {
-          if (val.hasOwnProperty(key_2)) {
-            var val_2 = val[key_2];
-            var id = key;
-            staff[`${val_2["Name"]}`] = id;
-          }
-        }
-      }
-    }
-  });
-  const schedule = () => {
-    db.ref(`staff/PickUp/${staff[staff_picker]}/`).push({
-      userId: userId,
-      orderId: orderId,
-    });
-    alert("Scheduled Staff successfully for Order No:" + orderId);
-    db.ref(`admin/ScheduledOrders`).push({
-      StaffId: staff[staff_picker],
-      userId: userId,
-      orderId: orderId,
-    });
-  };
+function CardSchedule({ userId, orderId, staffId }) {
+  console.log("------------" + staffId);
   return (
     <View style={styles.card}>
       <View style={styles.detailsContainer}>
@@ -60,48 +29,9 @@ function CardSchedule({ userId, orderId, genInvoice }) {
             marginTop: 10,
           }}
         >
-          Select Delivery Boy
+          STAFF ID
         </Text>
-        <Picker
-          selectedValue={staff_picker}
-          style={[styles.inputsingle, { height: 50, width: 200 }]}
-          onValueChange={(itemValue, itemIndex) => setstaff_picker(itemValue)}
-        >
-          <Picker.Item
-            label="Staff 1"
-            value="Staff 1"
-            style={styles.labelPicker}
-          />
-          <Picker.Item
-            label="Staff 2"
-            value="Staff 2"
-            style={styles.labelPicker}
-          />
-          <Picker.Item
-            label="Staff 3"
-            value="Staff 3"
-            style={styles.labelPicker}
-          />
-          <Picker.Item
-            label="Staff 4"
-            value="Staff 4"
-            style={styles.labelPicker}
-          />
-        </Picker>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <View style={styles.buttonStyle}>
-          <FormButton buttonTitle="Invoice" onPress={genInvoice} />
-        </View>
-        <View style={styles.buttonStyle}>
-          <FormButton
-            buttonTitle="Schedule"
-            onPress={() => {
-              schedule();
-            }}
-          />
-        </View>
+        <Text style={{ fontSize: 15, color: "#2f4f4f" }}>{staffId}</Text>
       </View>
     </View>
   );
@@ -121,11 +51,6 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 200,
-  },
-  buttonStyle: {
-    marginHorizontal: '5%',
-    marginVertical: 20,
-    width: '40%'
   },
 });
 
