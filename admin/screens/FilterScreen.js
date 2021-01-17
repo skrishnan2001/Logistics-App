4fccc9be5c7298cc3dfd54d25fd902bb45475e2e
unknown <krishwar2001@gmail.com>
2021-01-17 10:03:56 +0530

    did some corrections to filterscreen

--- admin/screens/FilterScreen.js
+++ admin/screens/FilterScreen.js
@@ -5,83 +5,90 @@ import { View, TextInput, StyleSheet, Text, } from "react-native";
 import FormInput from "../components/FormInput";
 
 function FilterScreen({ navigation }) {
-var users = [];
-
-  var dbRef = firebase.database().ref("/users/booking/");
-  dbRef.on("value", function (snapshot) {
-    const data = snapshot.val();
-    for (var key in data) {
-      if (data.hasOwnProperty(key)) {
-        var val = data[key];
-        for (var key_2 in val) {
-          if (val.hasOwnProperty(key_2)) {
-            var val_2 = val[key_2];
-            if (val_2["isScheduled"] == "Not Yet Scheduled") {
-              let user = {
-                id:
-                  new Date().getTime().toString() +
-                  Math.floor(
-                    Math.random() * Math.floor(new Date().getTime())
-                  ).toString(),
-                userid: key,
-                orderid: key_2,
-                city_pickup: val_2["city_pickup"],
-                state_pickup: val_2["state_pickup"],
-                city_delivery: val_2["city_delivery"],
-                state_delivery: val_2["state_delivery"],
-                pc_del: val_2["pincode_delivery"],
-                pc_pick: val_2["pincode_pickup"],
-                time: val_2["Time"],
-              };
-              users.push(user);
-            }
-          }
-        }
-      }
-    }
-  });
-
-
-
-
+  var users=[];
   var arrHolder = [];
   var volume="";
   var weight="";
   var searchtext="";
   var vehicle_type = "";
-
-  const filter_func = (text1) => {
-    const newData = users.filter((items) => {
-      var order = items.state_pickup;
-      var order1 = items.city_pickup;
-      var order2 = items.pc_pick;
-      return (
-        order.includes(text1) ||
-        order1.includes(text1) ||
-        order2.includes(text1)
-      );
-    });
-    arrHolder = newData;
-  };
-
-  const VehiclePicker = () => {
-    if (volume < 3 || weight < 5) {
-      vehicle_type = "two-wheeler";
-    } else if ((volume >= 3 && volume < 7) || (weight >= 5 && weight < 50)) {
-      vehicle_type = "four-wheeler";
-    } else if ((volume >= 7 && volume < 12) || (weight >= 50 && weight < 100)) {
-      vehicle_type = "mini-van";
-    } else {
-      vehicle_type = "truck";
-    }
-    console.log(vehicle_type);
-    console.log(weight,volume,searchtext);
-    filter_func(searchtext);
-    console.log("users:", users);
-    console.log("arrholder:",arrHolder);
+  
+  const userlist = () =>{
     users=[];
-    navigation.navigate("Sort", {vehicle_type: vehicle_type, arrHolder: arrHolder})
+    var dbRef = firebase.database().ref("/users/booking/");
+    dbRef.on("value", function (snapshot) {
+      const data = snapshot.val();
+      for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+          var val = data[key];
+          for (var key_2 in val) {
+            if (val.hasOwnProperty(key_2)) {
+              var val_2 = val[key_2];
+              if (val_2["isScheduled"] == "Not Yet Scheduled") {
+                let user = {
+                  id:
+                    new Date().getTime().toString() +
+                    Math.floor(
+                      Math.random() * Math.floor(new Date().getTime())
+                    ).toString(),
+                  userid: key,
+                  orderid: key_2,
+                  city_pickup: val_2["city_pickup"],
+                  state_pickup: val_2["state_pickup"],
+                  city_delivery: val_2["city_delivery"],
+                  state_delivery: val_2["state_delivery"],
+                  pc_del: val_2["pincode_delivery"],
+                  pc_pick: val_2["pincode_pickup"],
+                  time: val_2["Time"],
+                };
+                users.push(user);
+              }
+            }
+          }
+        }
+      }
+    });
   };
+  
+  const filter_func = (text1) => {
+      const newData = users.filter((items) => {
+        var order = items.state_pickup;
+        var order1 = items.city_pickup;
+        var order2 = items.pc_pick;
+        return (
+          order.includes(text1) ||
+          order1.includes(text1) ||
+          order2.includes(text1)
+        );
+      });
+      arrHolder=newData;
+  };
+  
+    const VehiclePicker = () => {
+      if (volume < 3 || weight < 5) {
+        vehicle_type = "two-wheeler";
+      } else if ((volume >= 3 && volume < 7) || (weight >= 5 && weight < 50)) {
+        vehicle_type = "four-wheeler";
+      } else if ((volume >= 7 && volume < 12) || (weight >= 50 && weight < 100)) {
+        vehicle_type = "mini-van";
+      } else {
+        vehicle_type = "truck";
+      }
+      console.log(vehicle_type);
+      console.log(weight,volume,searchtext);
+      userlist();
+      filter_func(searchtext);
+      console.log("users:", users);
+      console.log("arrholder:",arrHolder);
+      navigation.navigate("Sort", {vehicle_type: vehicle_type, arrHolder: arrHolder})
+    };
+  
+
+  
+
+
+
+
+ 
 
   return (
     <View
